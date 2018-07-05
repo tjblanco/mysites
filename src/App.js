56fab359923adc35a75b14f3bb0ce301be466c0a
Teresa Juan Blanco <teresa.juanblanco@genomcore.com>
2018-07-05 15:24:18 +0200

    Add hamburguer menu

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import menuIcon from './menu.svg'
+import './responsive.css'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">MySites in Gracia</h1>
+            <img src={menuIcon} className="menu" alt="menu" />
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">MySites in Gracia</h1>
         </header>
-        <body>
+          <body>
 
-        </body>
+          </body>
       </div>
     );
   }
