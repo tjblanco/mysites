420526fca1d4003daa394bc634306e96d4207896
Teresa Juan Blanco <teresa.juanblanco@genomcore.com>
2018-07-05 16:12:30 +0200

    Hide/diplay navigator accordingly

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import menuIcon from './menu.svg'
 import './responsive.css'
+import SitesList from "./SitesList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-            <img src={menuIcon} className="menu" alt="menu" />
+            <img onClick={() => (
+                document.querySelector('.nav_panel').classList.toggle('hidden')
+            )} src={menuIcon} className="menu" alt="menu" />
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">MySites in Gracia</h1>
         </header>
           <body>
-
+            <SitesList/>
           </body>
       </div>
     );
---
+++ src/SitesList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import App from "./App";
+
+class SitesList extends Component {
+    render() {
+        return(
+            <navigator className="nav_panel hidden">
+                <p>Here is the navigator panel!!</p>
+            </navigator>
+        )
+    }
+}
+
+export default SitesList;
