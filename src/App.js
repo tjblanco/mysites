783c9dc067fbff1ae2baf8ee589c5ac744496ee3
Teresa Juan Blanco <teresa.juanblanco@genomcore.com>
2018-07-06 12:08:23 +0200

    Update sites

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import Map from "./Map"
 class App extends Component {
     state = {
         sites: [
-            {lat: 41.402367, lng: 2.154347, title: 'La Teresa. Taverna Valenciana', class: 'Tapas'},
-            {lat: 41.3931733, lng: 2.1409225, title: 'Vins I Platillos Bar Restaurant', class: 'Tapas'},
+            {lat: 41.402367, lng: 2.154347, title: 'La Terreta. Taverna Valenciana', class: 'Tapas'},
+            {lat: 41.404103, lng: 2.156737, title: 'Vins I Platillos Bar Restaurant', class: 'Tapas'},
             {lat: 41.402952, lng: 2.15791, title: 'Bar Canigó', class: 'Tapas'},
             {lat: 41.4043009, lng: 2.1549702, title: 'Intrèpid de Gràcia', class: 'Tapas'},
             {lat: 41.4007937, lng: 2.1587425, title: 'Messié Pizza', class: 'Pizza'},
@@ -20,7 +20,10 @@ class App extends Component {
             {lat: 41.403539, lng: 2.160304, title: 'Heliogàbal', class: 'Cocktail Bar'},
             {lat: 41.4040643, lng: 2.1582468, title: 'Chatelet', class: 'Cocktail Bar'},
             {lat: 41.4040194, lng: 2.1581335, title: 'El Café de Teatre', class: 'Cocktail Bar'},
-            {lat: 41.404438, lng: 2.1568115, title: 'Gracia Latina', class: 'Cocktail Bar'},
+            {lat: 41.404438, lng: 2.1568115, title: 'Gràcia Latina', class: 'Cocktail Bar'},
+            {lat: 41.396620, lng: 2.156338, title: 'Modu', class: 'Asian'},
+            {lat: 41.399710, lng: 2.157236, title: 'Kibuka Sushi', class: 'Asian'},
+            {lat: 41.403824, lng: 2.148031, title: 'El Petit Bangkok', class: 'Asian'},
         ]
     }
   render() {
