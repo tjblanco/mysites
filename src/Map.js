a3c14f421c9566d37e1005342e5918062cf54bc1
Teresa Juan Blanco <teresa.juanblanco@genomcore.com>
2018-07-06 10:24:33 +0200

    Costum map options

--- src/Map.js
+++ src/Map.js
@@ -7,6 +7,16 @@ export default class Map extends Component {
         center: { lat: 41.40166, lng: 2.1566901 },
         zoom: 15
     }
+    createMapOptions= (maps) => {
+        return {
+            panControl: true,
+            mapTypeControl: false,
+            scrollwheel: false,
+            scaleControl: true,
+            styles: []
+        }
+    }
+
     render() {
         return (
             <div className='map'>
@@ -15,7 +25,9 @@ export default class Map extends Component {
                     defaultZoom={ this.props.zoom }
                     bootstrapURLKeys={{
                         key: 'AIzaSyASXg9YtZ5fVwJQk3zPGQuaina7Qqt5ZEE'
-                    }}>
+                    }}
+                    options={this.createMapOptions}
+                >
 
                     <GoogleMap
 
